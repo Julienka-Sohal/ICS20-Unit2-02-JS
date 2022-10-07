e95874074f30771c91a7de2d59040fc4370262db
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-10-07 18:29:40 +0000

    program

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Julienka Sohal All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+
+function doMathClicked () {
+  document.getElementById("area-math").innerHTML = "<p> The area for this rectangle is " + (5 * 3) + " cm² <p>"
+  document.getElementById("perimeter-math").innerHTML = "<p> The perimerter is = " + (2*(5 + 3)) + " cm </p>"
+}
