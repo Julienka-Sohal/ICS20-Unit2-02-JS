fae162f92c14ed92628554ea31a699cba74f4358
Julienka-Sohal <Julienka-Sohal@users.noreply.github.com>
2022-10-07 18:31:30 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@
  * Do basic math.
  */
 
-function doMathClicked () {
-  document.getElementById("area-math").innerHTML = "<p> The area for this rectangle is " + (5 * 3) + " cm² <p>"
-  document.getElementById("perimeter-math").innerHTML = "<p> The perimerter is = " + (2*(5 + 3)) + " cm </p>"
+function doMathClicked() {
+  document.getElementById("area-math").innerHTML =
+    "<p> The area for this rectangle is " + 5 * 3 + " cm² <p>"
+  document.getElementById("perimeter-math").innerHTML =
+    "<p> The perimerter is = " + 2 * (5 + 3) + " cm </p>"
 }
