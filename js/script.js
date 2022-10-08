36235d697c6069d702d9252a117f8bd6e3cf6cbd
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-10-08 03:53:04 +0000

    code

--- js/script.js
+++ js/script.js
@@ -12,5 +12,5 @@ function doMathClicked() {
   document.getElementById("area-math").innerHTML =
     "<p> The area for this rectangle is " + 5 * 3 + " cm² <p>"
   document.getElementById("perimeter-math").innerHTML =
-    "<p> The perimerter is = " + 2 * (5 + 3) + " cm </p>"
+    "<p> The perimeter is = " + 2 * (5 + 3) + " cm </p>"
 }
